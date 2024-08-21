28b954b3d8f2e5aea5204efc75ae9e69bec6cf09
AvikNayak22 <nayakavik19@gmail.com>
2024-08-21 21:10:58 +0530

    basic initialization

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 // run `node index.js` in the terminal
+const app = require("express")();
+const http = require("http").Server(app);
+const path = require("path");
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const io = require("socket.io")(http);
+
+app.get("/", function (req, res) {
+  const options = {
+    root: path.join(__dirname),
+  };
+
+  const fileName = "index.html";
+
+  res.sendFile(fileName, options);
+});
+
+io.on("connection", function (socket) {
+  console.log("A user connected");
+
+  socket.on("disconnect", function () {
+    console.log("A user disconnected");
+  });
+});
+
+http.listen(3000, function () {
+  console.log("Server ready on 3000");
+});
